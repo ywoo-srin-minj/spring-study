d30f039b564260d14c8cd7551d74618bf8112522
LEE MINJI <m04j00@gmail.com>
2022-02-04 23:01:36 +0900

    [add] (#3) 기본 파일 세팅

---
+++ src/main/java/controller/user_minj_controller.java
@@ -0,0 +1,9 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users-minj")
+public class user_minj_controller {
+}
