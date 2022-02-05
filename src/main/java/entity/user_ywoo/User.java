582a3366bdea775c596a4dcacf8f902cab0cd220
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-05 20:02:25 +0900

    [add] (#4) User 엔티티 설계 - 연우

--- src/main/java/entity/user_ywoo/User.java
+++ src/main/java/entity/user_ywoo/User.java
@@ -1,4 +1,25 @@
 package entity.user_ywoo;
 
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name="user")
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //AUTO_INCREMENT
+    private Long id;
+
+    @Column(length = 30, unique = true, nullable = false, name = "user_id")
+    private Long user_id;
+
+    @Column(length = 10, nullable = false, name = "user_name")
+    private String user_name;
+
+    @Column(length = 30, nullable = false, name = "user_password")
+    private String user_password;
+
 }
