9b1b7e3f420fa1b72024a8cfff976dfb4dff0fae
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-07 00:22:01 +0900

    [add] (#8) 회원 추가 기능 구현 - 연우

--- src/main/java/com/second/spring_study/controller/user_ywoo_controller.java
+++
@@ -1,9 +0,0 @@
-package com.second.spring_study.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/users-ywoo")
-public class user_ywoo_controller {
-}
