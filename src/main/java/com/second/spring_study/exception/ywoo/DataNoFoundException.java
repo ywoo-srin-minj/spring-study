5fbdf2bedd49f6e9b3dbe70f86aee17276440bcd
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-08 01:08:11 +0900

    [add] (#12) DataNotFoundException

---
+++ src/main/java/com/second/spring_study/exception/ywoo/DataNoFoundException.java
@@ -0,0 +1,14 @@
+package com.second.spring_study.exception.ywoo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DataNoFoundException extends RuntimeException{
+
+    private static final String message = "해당하는 id를 찾을 수 없음";
+    public DataNoFoundException(){
+        super(message);
+
+    }
+}
