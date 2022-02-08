8ddf54ba2102946ffb66d2f6b1f3a4ea54323c21
SRin23 <serin4723@naver.com>
2022-02-08 22:46:02 +0900

    [add](#09)회원 전체 조회 구현 - 세린

--- src/main/java/com/second/spring_study/controller/UserSrinController.java
+++ src/main/java/com/second/spring_study/controller/UserSrinController.java
@@ -1,9 +1,13 @@
 package com.second.spring_study.controller;
 
 import com.second.spring_study.dto.request.srin.UserRequestDto;
+import com.second.spring_study.dto.request.srin.UserResponseDto;
+import com.second.spring_study.entity.user_srin.UserSrin;
 import com.second.spring_study.service.UserSrinService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserSrinController {
     private final UserSrinService userSrinService;
 
-    @PostMapping()
+    @PostMapping("/")
     public void createUser(@RequestBody UserRequestDto userRequestDto) {
         userSrinService.createUser(userRequestDto);
     }
@@ -19,5 +23,20 @@ public class UserSrinController {
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable long id) {
         userSrinService.deleteUser(id);
+    }
+
+    @ResponseBody
+    @GetMapping("")
+    public List<UserResponseDto> findAllUser(){
+        /*
+        List<UserSrin> allUser = userSrinService.findAllUser();
+        List<UserResponseDto> userList = new ArrayList<UserResponseDto>();
+        for(UserSrin us:allUser){
+            userList.add(new UserResponseDto(us.getId(), us.getUser_id(), us.getUser_password(), us.getUser_name()));
+        }
+        return userList;
+        */
+        List<UserResponseDto> userList = userSrinService.findAllUser();
+        return userList;
     }
 }
--- src/main/java/com/second/spring_study/service/UserSrinService.java
+++ src/main/java/com/second/spring_study/service/UserSrinService.java
@@ -1,11 +1,21 @@
 package com.second.spring_study.service;
 
 import com.second.spring_study.dto.request.srin.UserRequestDto;
+import com.second.spring_study.dto.request.srin.UserResponseDto;
 import com.second.spring_study.entity.user_srin.UserSrin;
 import com.second.spring_study.entity.user_srin.repository.UserSrinRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.expression.Lists;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 //실제 기능 구현하는곳
 @RequiredArgsConstructor
@@ -28,4 +38,17 @@ public class UserSrinService {
         */
         userRepository.deleteById(id);
     }
+
+    @Transactional
+    public List<UserResponseDto> findAllUser(){
+        List<UserSrin> allUser = (List<UserSrin>) userRepository.findAll();
+
+
+        List<UserResponseDto> stream = (List<UserResponseDto>) allUser.stream()
+                .map(userSrin -> new UserResponseDto(userSrin.getId(), userSrin.getUser_id(), userSrin.getUser_password(), userSrin.getUser_name()))
+                .collect(Collectors.toList());
+
+        return stream;
+    }
+
 }
