4cede2335479416b7ece7b1ea8c1de05321dad09
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-10 02:24:11 +0900

    [update] (#10) @setter 삭제 - 연우

--- src/main/java/com/second/spring_study/dto/request/ywoo/UserRequestUpdateDto.java
+++ src/main/java/com/second/spring_study/dto/request/ywoo/UserRequestUpdateDto.java
@@ -4,7 +4,6 @@ import com.sun.istack.NotNull;
 import lombok.*;
 
 @Getter
-@Setter
 @AllArgsConstructor //생성자 생성
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserRequestUpdateDto {
