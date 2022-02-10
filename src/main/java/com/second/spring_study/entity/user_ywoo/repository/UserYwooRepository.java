fa0903f1f98245ffade9d04262da6ab23aff6dec
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-11 00:33:11 +0900

    [update] (#26) Custom Exception 적용 - 연우

--- src/main/java/com/second/spring_study/entity/user_ywoo/repository/UserYwooRepository.java
+++ src/main/java/com/second/spring_study/entity/user_ywoo/repository/UserYwooRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserYwooRepository extends CrudRepository<UserYwoo, Long>, UserYwooRepositoryExtension {
     UserYwoo save(UserYwoo userYwoo);
+    Boolean existsByUserId(String userId);
 }
