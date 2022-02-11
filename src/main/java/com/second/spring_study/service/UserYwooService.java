199edd056782b64a26f6a6fece6f8f0773890333
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-12 03:15:05 +0900

    [add] (#27) UserController 통합 테스트 코드 작성 (#38)
    * [update] (#26) 회원 수정 기능 수정 - 연우
    
    * [add] (#26) 테스트 h2 데이터 추가 - 연우
    
    * [add] (#26) application.yml 파일 재생성 - 연우
    
    * [add] (#26) controller 테스트 코드 추가- 연우

--- src/main/java/com/second/spring_study/service/UserYwooService.java
+++ src/main/java/com/second/spring_study/service/UserYwooService.java
@@ -65,6 +65,6 @@ public class UserYwooService {
         userRepository.findById(id).orElseThrow(()->{
             throw new ApiExceptionYwoo(ErrorCodeEnum.USER_NOT_FOUND);
         });
-        updateUser(id,userRequestUpdateDto);
+        userRepository.updateUser(id,userRequestUpdateDto);
     }
 }
