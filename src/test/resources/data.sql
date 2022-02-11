7d93eae7a3828776dabc740d61430ef200884fc6
LEE MINJI <m04j00@gmail.com>
2022-02-12 01:14:49 +0900

    [ADD] (#27) test에서 h2 디비 연결, 더미 데이터 추가 - 민지

---
+++ src/test/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user_minj values(1, 'id001', '001', 'password');
+INSERT INTO user_minj values(2, 'id002', '002', 'password');
---
+++ src/test/resources/table.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE user_minj
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    user_id       VARCHAR(30) NOT NULL UNIQUE,
+    user_name     VARCHAR(10) NOT NULL,
+    user_password VARCHAR(30) NOT NULL
+);
