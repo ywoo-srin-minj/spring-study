833b90e3608030cc022e3473217774308717fa37
LEE MINJI <m04j00@gmail.com>
2022-02-12 01:20:25 +0900

    [UPDATE] (#27) 더미데이터 수정 - 민지

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO user_minj values(1, 'id001', '001', 'password');
 INSERT INTO user_minj values(2, 'id002', '002', 'password');
+INSERT INTO user_srin values(1, 'id001', '001', 'password');
+INSERT INTO user_srin values(2, 'id002', '002', 'password');
+INSERT INTO user_ywoo values(1, 'id001', '001', 'password');
+INSERT INTO user_ywoo values(2, 'id002', '002', 'password');
