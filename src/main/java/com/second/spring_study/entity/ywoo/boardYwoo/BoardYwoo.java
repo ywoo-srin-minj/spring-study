8c0e0161c35ab32cd813a422c827e5037a36e719
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-15 00:36:54 +0900

    [ADD] (#39) Board entity 추가 - 연우

---
+++ src/main/java/com/second/spring_study/entity/ywoo/boardYwoo/BoardYwoo.java
@@ -0,0 +1,29 @@
+package com.second.spring_study.entity.ywoo.boardYwoo;
+
+import com.second.spring_study.entity.BaseEntity;
+import com.second.spring_study.entity.ywoo.userYwoo.UserYwoo;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table
+public class BoardYwoo extends BaseEntity {
+
+    @Id
+    @Column(name="board_id", nullable = false)
+    private long id;
+
+    @Column(name="board_title",nullable = false)
+    private String boardTitle;
+
+    @Column(name = "board_content",nullable = false)
+    private String boardContent;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private UserYwoo userYwoo;
+
+}
