e976706df3bae3509d4b1f4188d320ada078324d
LEE MINJI <m04j00@gmail.com>
2022-02-15 00:36:01 +0900

    [CREATE] (#39) BaseEntity 생성 - 민지

---
+++ src/main/java/com/second/spring_study/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.second.spring_study.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners({AuditingEntityListener.class})
+@Getter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false, name="created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name="last_modified_at")
+    private LocalDateTime lastModifiedAt;
+}
