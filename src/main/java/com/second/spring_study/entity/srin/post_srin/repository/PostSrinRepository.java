137756302c2535c706f79894b25ebb49af975bbb
SRin23 <serin4723@naver.com>
2022-02-15 23:57:44 +0900

    [update](#40) save 주석 처리 - 세린

--- src/main/java/com/second/spring_study/entity/srin/post_srin/repository/PostSrinRepository.java
+++ src/main/java/com/second/spring_study/entity/srin/post_srin/repository/PostSrinRepository.java
@@ -4,6 +4,8 @@ import com.second.spring_study.entity.srin.post_srin.PostSrin;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostSrinRepository extends CrudRepository<PostSrin, Long> {
-    @Override
-    PostSrin save(PostSrin postSrin);
+
+//save는 내장 메서드 이므로 따로 명시하지 않아도 된다.
+//    @Override
+//    PostSrin save(PostSrin postSrin);
 }
