5fa47432f839a2c17cc4bd1984e9a4072e1bfe49
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-19 22:02:47 +0900

    [UPDATE] (#46) 3.0.0 버전 변경 - 연우

--- src/main/java/com/second/spring_study/config/SwaggerConfig.java
+++ src/main/java/com/second/spring_study/config/SwaggerConfig.java
@@ -2,34 +2,27 @@ package com.second.spring_study.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
+
     @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30) // open api spec 3.0
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                //사용할 패키지의 경로를 작성한다.
-                //any는 다 사용한다는 뜻이다.
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(metaData());
-        //해당 swagger페이지의 정보?를 적는다.
-        //new ApiInfo로 바로 작성할 수도 있다.
+                .apiInfo(apiInfo());
 
     }
-    private ApiInfo metaData() {
+    private ApiInfo apiInfo(){
         return new ApiInfoBuilder()
                 .title("Spring Boot Swagger App")
                 //title은 화면에 보여질 제목이다.
@@ -38,14 +31,5 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .version("1.0.0")
                 //해당 api의 버전이다.
                 .build();
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
