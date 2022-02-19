c7f8b34ed6228a3257956c428db9af5b3e0d34ff
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-19 18:09:15 +0900

    [ADD] (#45) controller 테스트 코드 작성 - 연우

--- src/test/resources/table.sql
+++ src/test/resources/table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user_minj
+CREATE TABLE IF NOT EXISTS user_minj
 (
     id            INT AUTO_INCREMENT PRIMARY KEY,
     user_id       VARCHAR(30) NOT NULL UNIQUE,
@@ -6,7 +6,7 @@ CREATE TABLE user_minj
     user_password VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE user_srin
+CREATE TABLE IF NOT EXISTS user_srin
 (
     id            INT AUTO_INCREMENT PRIMARY KEY,
     user_id       VARCHAR(30) NOT NULL UNIQUE,
@@ -14,10 +14,22 @@ CREATE TABLE user_srin
     user_password VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE user_ywoo
+CREATE TABLE IF NOT EXISTS user_ywoo
 (
     id            INT AUTO_INCREMENT PRIMARY KEY,
     user_id       VARCHAR(30) NOT NULL UNIQUE,
     user_name     VARCHAR(10) NOT NULL,
     user_password VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS post_ywoo
+(
+    post_id INT AUTO_INCREMENT PRIMARY KEY,
+    post_title VARCHAR(30) NOT NULL,
+    post_content TEXT NOT NULL,
+    created_at DATE NOT NULL,
+    last_modified_at DATE NOT NULL,
+    user_id INT AUTO_INCREMENT,
+    FOREIGN KEY (user_id)
+    REFERENCES user_ywoo(id) ON DELETE CASCADE
+    );
