055cf5557fb9c353fabe7dc2896997370be1e658
Min J <69101860+m04j00@users.noreply.github.com>
2022-02-19 20:05:35 +0900

    [ADD] (#45) Post Controller 테스트 - 민지 (#61)

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -4,3 +4,5 @@ INSERT INTO user_srin values(1, 'id001', '001', 'password');
 INSERT INTO user_srin values(2, 'id002', '002', 'password');
 INSERT INTO user_ywoo values(1, 'id001', '001', 'password');
 INSERT INTO user_ywoo values(2, 'id002', '002', 'password');
+INSERT INTO post_minj values(1, current_date(), current_date(), '1. title', 'content',  1);
+INSERT INTO post_minj values(2, current_date(), current_date(), '2. title', 'content',  2);
